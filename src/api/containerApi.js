ab4b67e11f3c96035735034d39d4671d3f0e84f5
hyejin0904 <imagine@kookmin.ac.kr>
2025-07-02 15:11:47 +0900

    Feat: 컨테이너 목록 관련 임시 API 연결부 작성

--- src/api/containerApi.js
+++ src/api/containerApi.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+// import axios from 'axios';
 import dummyData from '../data/dummyContainerData.json';
 
-const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+// const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 // 컨테이너 목록 조회 API 호출
 export const getContainerList = () => {
@@ -20,3 +20,34 @@ export const getContainerList = () => {
 // }
 //
 
+export const startContainer = ({userId, userPw, serverName}) => {
+    console.log(`Start Container: ${serverName}`);
+    return Promise.resolve({success : true});
+    //     return axios.get(`${BASE_URL}/api/`, {
+    //     params: { userId, userPw, serverName}
+    // })
+}
+
+export const stopContainer = ({userId, userPw, serverName}) => {
+    console.log(`Stop Container: ${serverName}`);
+    return Promise.resolve({success : true});
+    //     return axios.get(`${BASE_URL}/api/`, {
+    //     params: { userId, userPw, serverName}
+    // })
+}
+
+export const fetchLogs = ({userId, userPw, serverName}) => {
+    console.log(`Stop Container: ${serverName}`);
+    return Promise.resolve({logs: `${serverName} 로그 내용`});
+    //     return axios.get(`${BASE_URL}/api/`, {
+    //     params: { userId, userPw, serverName}
+    // })
+}
+
+export const deleteContainer = ({userId, userPw, serverName}) => {
+    console.log(`Delete Container: ${serverName}`);
+    return Promise.resolve({success : true});
+    //     return axios.delete(`${BASE_URL}/api/`, {
+    //     data: { userId, userPw, serverName}
+    // })
+}
