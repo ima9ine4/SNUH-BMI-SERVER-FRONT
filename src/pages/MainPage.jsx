3b534dfcd5633be1af93977e21cff4251f8e6b0a
HYEJIN LIM <imagine@kookmin.ac.kr>
2025-07-24 11:44:19 +0900

    Feat: 파일 목록 UI 추가 및 기능 연결

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -6,17 +6,19 @@ import NewContainerModal from '../components/NewContainerModal'
 import PasswordChangeModal from '../components/PasswordChangeModal'
 import { BsDownload } from 'react-icons/bs';
 import { LuRefreshCw } from "react-icons/lu";
-import { getDownloadList } from '../api/downloadApi';
 import { MdOutlineReplay } from "react-icons/md";
 import { FaRegCircleStop } from "react-icons/fa6";
 import ContainerSkeletonRow from '../components/skeleton/ContainerSkeletonRow';
+import FileListSkeletonRow from '../components/skeleton/FileListSkeletonRow';
 import { changePassword } from '../api/loginApi';
+import { getFileList } from '../api/FileApi';
+import dayjs from 'dayjs';
 
 
 const COMPANY_NAME = 'SNUH BMI LAB SERVER';
 
 const PAGE_SIZE = 4;
-const DOWNLOAD_PAGE_SIZE = 4;
+const FILE_PAGE_SIZE = 4;
 
 function mapApiContainer(apiObj) { // api response의 원본 json 배열을 가공하여 저장
     return {
@@ -45,10 +47,11 @@ const MainPage = ({ user, onLogout }) => {
     const totalPages = Math.ceil(containerData.length / PAGE_SIZE);
     const pagedData = containerData.slice((page - 1) * PAGE_SIZE, page * PAGE_SIZE);
 
-    const [downloadData, setDownloadData] = useState([]);
-    const [downloadPage, setDownloadPage] = useState(1);
-    const downloadTotalPages = Math.ceil(downloadData.length / DOWNLOAD_PAGE_SIZE);
-    const downloadPagedData = downloadData.slice((downloadPage - 1) * DOWNLOAD_PAGE_SIZE, downloadPage * DOWNLOAD_PAGE_SIZE);
+    const [FileData, setFileData] = useState([]);
+    const [getFileListLoading, setGetFileListLoading] = useState(true);
+    const [filePage, setFilePage] = useState(1);
+    const fileTotalPages = Math.ceil(FileData.length / FILE_PAGE_SIZE);
+    const filePagedData = FileData.slice((filePage - 1) * FILE_PAGE_SIZE, filePage * FILE_PAGE_SIZE);
 
     // 컨테이너 목록 새로고침 함수
     const refreshContainerList = () => {
@@ -60,7 +63,6 @@ const MainPage = ({ user, onLogout }) => {
                 setLoading(false);
             })
             .catch((err) => {
-                
                 setLoading(false);
             });
     };
@@ -142,6 +144,25 @@ const MainPage = ({ user, onLogout }) => {
             });
     };
 
+    // 파일 목록 새로고침 함수
+    const refreshFileList = () => {
+        setGetFileListLoading(true);
+        getFileList({userId: user.userId, userPW: user.userPW})
+            .then((res) => {
+                const data = res.data.data.item_list;
+                const convertedDateData = data.map(item => ({
+                    ...item,
+                    allowed_date: dayjs(item.allowed_date).add(9, 'hour').format('YYYY-MM-DD HH:mm:ss'),
+                    upload_date: dayjs(item.upload_date).add(9, 'hour').format('YYYY-MM-DD HH:mm:ss'),
+                }))
+                setFileData(convertedDateData);
+                setGetFileListLoading(false);
+            })
+            .catch((err) => {
+                setGetFileListLoading(false);
+            });
+    };
+
     const handleChangePassword = (new_password) => {  // 비밀번호 변경 API 호출
         changePassword({userId: user.userId, userPW: user.userPW, new_password})
             .then(() => {
@@ -154,16 +175,10 @@ const MainPage = ({ user, onLogout }) => {
                 alert("비밀번호 변경에 실패하였습니다.");  
             })
     }
-    
-    // 다운로드 데이터 불러오기
-    const fetchDownloadData = () => {
-        getDownloadList().then(res => {
-            setDownloadData(res.data);
-        });
-    };
 
     useEffect(() =>{
         refreshContainerList();
+        refreshFileList();
         getDockerVolume({userId: user.userId, userPW: user.userPW}).then((response) => {
             if(response.data.status_code !== 404){ // 도커 볼륨이 있을 때만
                 setAvailableVolumes(response.data );
@@ -172,7 +187,6 @@ const MainPage = ({ user, onLogout }) => {
         .catch((err) => {
             console.log("도커 볼륨 로딩 실패", err);
         })
-        fetchDownloadData();
     }, [user.userId, user.userPW]);
 
     useEffect(() => {
@@ -372,21 +386,21 @@ const MainPage = ({ user, onLogout }) => {
             )}
 
             {/* 다운로드 목록 헤더 */}
-            {/* <div className="max-w-7xl mx-auto flex justify-between items-center px-3 mt-8 mb-4">
+            <div className="max-w-7xl mx-auto flex justify-between items-center px-3 mt-8 mb-4">
                 <div className="flex gap-2">
                     <span className="text-3xl font-medium text-gray-900 mb-2">파일 다운로드</span>
                     <button
                         className="p-2 rounded-full hover:bg-gray-100 text-gray-400 mb-2"
                         title="파일 다운로드 목록 새로고침"
-                        onClick={fetchDownloadData}
+                        onClick={refreshFileList}
                     >
                         <LuRefreshCw size={18} />
                     </button>
                 </div>
-            </div> */}
+            </div>
 
             {/* 다운로드 목록 테이블 */}
-            {/* <div className="max-w-7xl mx-auto bg-white rounded-xl shadow-md border border-gray-200 mb-10 overflow-x-auto">
+            <div className="max-w-7xl mx-auto bg-white rounded-xl shadow-md border border-gray-200 mb-10 overflow-x-auto">
                 <table className="w-full min-w-[900px] text-xs sm:text-sm table-fixed">
                 <colgroup>
                     <col className="w-16" />
@@ -405,44 +419,51 @@ const MainPage = ({ user, onLogout }) => {
                     </tr>
                 </thead>
                 <tbody>
-                    {downloadPagedData.map((c) => (
-                    <tr key={c.id} className="group border-b border-gray-100 last:border-0 hover:bg-blue-50/60 transition">
-                        <td className="py-3 px-2 align-middle text-center text-gray-700 truncate">{c.requestDate}</td>
-                        <td className="py-3 px-2 align-middle text-center font-semibold text-gray-700 truncate">{c.fileName}</td>
-                        <td className="py-3 px-2 align-middle text-center">
-                            <span
-                                className={`inline-block px-2.5 py-1 rounded-full text-xs font-semibold border
-                                    ${c.status === '허가 완료' ? 'bg-green-50 text-green-700 border-green-200' :
-                                        c.status === '대기 중' ? 'bg-yellow-50 text-yellow-700 border-yellow-200' :
-                                        c.status === '반려' ? 'bg-red-50 text-red-600 border-red-200' :
-                                        ''}
-                                `}
-                            >
-                                {c.status}
-                            </span>
-                        </td>
-                        <td className="py-3 px-2 align-middle text-center text-gray-700">{c.approveDate || '-'}</td>
-                        <td className="py-3 px-2 align-middle text-center">
-                            {c.status === '허가 완료' ? (
-                              <button className="p-1 rounded hover:bg-gray-100 text-gray-500 text-sm" title="다운로드"
-                                  onClick={() => alert(`${c.fileName} 다운로드`)}>
-                                  <BsDownload />
-                              </button>
-                            ) : (
-                              <span className="text-gray-400">-</span>
-                            )}
-                        </td>
-                    </tr>
-                    ))}
+                    { getFileListLoading
+                        ? Array.from({ length: 4 }).map((_, idx) => <FileListSkeletonRow key={idx} />)
+                        : filePagedData.map((c) => (
+                            <tr key={c.id} className="group border-b border-gray-100 last:border-0 hover:bg-blue-50/60 transition">
+                                <td className="py-3 px-2 align-middle text-center text-gray-700 truncate">{c.upload_date}</td>
+                                <td className="py-3 px-2 align-middle text-center font-semibold text-gray-700 truncate">{c.name}</td>
+                                <td className="py-3 px-2 align-middle text-center">
+                                    <span
+                                        className={`inline-block px-2.5 py-1 rounded-full text-xs font-semibold border
+                                            ${c.download_allowed ? 'bg-green-50 text-green-700 border-green-200' :
+                                                !c.download_allowed ? 'bg-yellow-50 text-yellow-700 border-yellow-200' :
+                                                ''}
+                                        `}
+                                    >
+                                        {c.download_allowed
+                                            ? "승인"
+                                            :"미승인"}
+                                    </span>
+                                </td>
+                                <td className="py-3 px-2 align-middle text-center text-gray-700">
+                                    {c.download_allowed
+                                        ? c.allowed_date
+                                        : '-'}
+                                </td>
+                                <td className="py-3 px-2 align-middle text-center">
+                                    {c.download_allowed ? (
+                                    <button className="p-1 rounded hover:bg-gray-100 text-gray-500 text-sm" title="다운로드"
+                                        onClick={() => alert(`${c.name} 다운로드`)}>
+                                        <BsDownload />
+                                    </button>
+                                    ) : (
+                                    <span className="text-gray-400">-</span>
+                                    )}
+                                </td>
+                            </tr>
+                        ))}
                 </tbody>
-                </table> */}
+                </table>
                 {/* 페이지네이션 */}
-                {/* <div className="flex justify-center items-center gap-4 py-4">
-                    <button onClick={() => setDownloadPage(downloadPage - 1)} disabled={downloadPage === 1} className="px-3 py-1 rounded bg-gray-100 text-gray-500 disabled:opacity-50 disabled:cursor-not-allowed">&lt;</button>
-                    <span className="text-gray-700 text-xs sm:text-sm">{downloadPage} / {downloadTotalPages}</span>
-                    <button onClick={() => setDownloadPage(downloadPage + 1)} disabled={downloadPage === downloadTotalPages} className="px-3 py-1 rounded bg-gray-100 text-gray-500 disabled:opacity-50 disabled:cursor-not-allowed">&gt;</button>
+                <div className="flex justify-center items-center gap-4 py-4">
+                    <button onClick={() => setFilePage(filePage - 1)} disabled={filePage === 1} className="px-3 py-1 rounded bg-gray-100 text-gray-500 disabled:opacity-50 disabled:cursor-not-allowed">&lt;</button>
+                    <span className="text-gray-700 text-xs sm:text-sm">{filePage} / {fileTotalPages}</span>
+                    <button onClick={() => setFilePage(filePage + 1)} disabled={filePage === fileTotalPages} className="px-3 py-1 rounded bg-gray-100 text-gray-500 disabled:opacity-50 disabled:cursor-not-allowed">&gt;</button>
                 </div>
-            </div> */}
+            </div>
         </div>
     );
 }
