8f46b70a60edf5f1ba13da737d5c4697f79b1bd5
HYEJIN LIM <imagine@kookmin.ac.kr>
2025-07-24 11:43:20 +0900

    Feat: 파일 목록 Skeleton 추가

---
+++ src/components/skeleton/FileListSkeletonRow.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FileListSkeletonRow = () => (
+    <tr classname="animate-pulse">
+        {Array.from({ length: 5}).map((__dirname, idx) => (
+            <td key={idx} className="py-4 px-6">
+                <div className="h-5 bg-gray-100 rounded w-full"></div>
+            </td>
+        ))}
+    </tr>
+);
+
+export default FileListSkeletonRow;
