22995478a92acbe84dc0c407dcc8ddb4ff1b38be
HYEJIN LIM <imagine@kookmin.ac.kr>
2025-07-24 17:47:01 +0900

    Chore: alert 문구 수정

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -191,11 +191,11 @@ const MainPage = ({ user, onLogout }) => {
                 onLogout();
             })
             .catch((err) => {
-                alert("비밀번호 변경에 실패하였습니다.");  
+                alert("비밀번호 변경에 실패하였습니다. 다시 시도해주세요.");  
             })
     }
 
-    useEffect(() =>{
+    useEffect(() => {
         refreshContainerList();
         refreshFileList();
         getDockerVolume({userId: user.userId, userPW: user.userPW}).then((response) => {
