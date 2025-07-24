ea57dc6c5eec73fd07ceda77de58b8056bd53fc2
HYEJIN LIM <imagine@kookmin.ac.kr>
2025-07-24 17:42:15 +0900

    Feat: 파일 업로드 API 호출부 구현

--- src/api/FileApi.js
+++ src/api/FileApi.js
@@ -14,3 +14,24 @@ export const getFileList = ({ userId, userPW }) => {
             }
         )
 }
+
+export const UploadFile = ({ userId, userPW, files }) => {
+    const formData = new FormData();
+    files.forEach((files) => {
+        formData.append("files", files);
+    })
+
+    return axios.post(`${BASE_URL}/file_io/upload_file`, formData, {
+        headers: {
+            'Content-Type': 'multipart/form-data',
+            'accept': 'application/json',
+            'userID': userId,
+            'userPW': userPW
+            }
+        })
+        .catch(error => {
+            console.log(error.response.data.errors);
+            }
+        )
+}
+}
