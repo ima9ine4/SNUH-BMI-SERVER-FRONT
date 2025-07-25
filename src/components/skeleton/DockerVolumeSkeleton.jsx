3057ac4bab62f39f2d9c0d290a4cf116e1148f02
HYEJIN LIM <imagine@kookmin.ac.kr>
2025-07-25 16:22:32 +0900

    Feat: 도커 볼륨 Skeleton 추가

---
+++ src/components/skeleton/DockerVolumeSkeleton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DockerVolumeSkeletonRow = () => (
+    <tr classname="animate-pulse">
+        {Array.from({ length: 3}).map((__dirname, idx) => (
+            <td key={idx} className="py-4 px-6">
+                <div className="h-5 bg-gray-100 rounded w-full"></div>
+            </td>
+        ))}
+    </tr>
+);
+
+export default DockerVolumeSkeletonRow;
