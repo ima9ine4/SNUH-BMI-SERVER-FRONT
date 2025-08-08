78632e1e51c3c4f82652f2687887bf5f9af00b6f
HYEJIN LIM <imagine@kookmin.ac.kr>
2025-08-08 16:13:36 +0900

    Feat: 파일 다운로드 신청 목록 조회 API 호출부 구현

--- src/api/admin/userApi.js
+++ src/api/admin/userApi.js
@@ -8,4 +8,18 @@ export const getAllUsers = (userPW) => {
             'BMI_SWAGGER_KEY': userPW,
         }
     });
+
+// 특정 사용자의 파일 다운로드 신청 목록 조회
+export const getUserDownloadRequests = ({userPW, user_id}) => {
+    return axios.get(`${ADMIN_URL}/Client_Download/client_file_list`, {
+        params: {
+            'userID': user_id
+        },
+        headers: {
+            'accept': 'application/json',
+            'BMI_SWAGGER_KEY': userPW,
+        }
+    });
+}
+
 }
