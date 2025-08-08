6b98c44e517cbbcbea4cb8dfd632238969d6b59d
HYEJIN LIM <imagine@kookmin.ac.kr>
2025-08-08 10:36:37 +0900

    Feat: 볼륨 생성 API 호출 수정

--- src/api/admin/volumeControlApi.js
+++ src/api/admin/volumeControlApi.js
@@ -1,17 +1,19 @@
 import axios from 'axios';
 const ADMIN_URL = process.env.REACT_APP_API_ADMIN_URL;
 
-export const CreateVolumeApi = ({ userPW, abs_path, readwrite, users })=> {
+export const CreateVolumeApi = ({ userPW, volumeData })=> {
+    let vol_name = volumeData.vol_name;
     const requestBody = {
-        'abs_path': abs_path,
-        'readwrite' : readwrite,
-        'users' : users
+        'abs_path': volumeData.abs_path,
+        'readwrite' : volumeData.readwrite,
+        'users': volumeData.users
     };
-    return axios.post(`${ADMIN_URL}/volume_control/volumes/{volume_name}`, requestBody, {
+    return axios.post(`${ADMIN_URL}/volume_control/volumes/${vol_name}`, requestBody, {
         headers: {
             'accept': 'application/json',
             'BMI_SWAGGER_KEY': userPW,
             'Content-Type' : 'application/json'
         }
     });
+}
 }
